d3cc06402fdb9db18f404b37299af92307e0d243
CarlosEdson <carlosedsonduarte@gmail.com>
2024-07-24 20:12:08 -0300

    teste

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const alterarusuario = document.querySelector('#menu')
 
-function alteracaodeusuario(){
+function trocadeusuario(){
     if(menu.style.display == 'block'){
         menu.style.display = 'none'
     } else {
         menu.style.display = 'block'
     }
 }
-document.addEventListener('click',(Event) => {
-    console.log('opa')
+
+document.addEventListener('click', (Event) => {
+    console.log(Event.target)
+    if(menu.contains(Event.target) && menu.querySelector('#menu')){
+        trocadeusuario()
+    }
 })
