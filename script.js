c9c252849590870e7e155b8227c99156b1f147b6
CarlosEdson <carlosedsonduarte@gmail.com>
2024-08-13 12:42:24 -0300

    a

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const abrir = document.querySelector('#menu')
+const corpo = document.querySelector('#corpo')
 
 function abrirmenu(){
     if(abrir.style.display == 'block'){
         abrir.style.display = 'none'
     } else {
         abrir.style.display = 'block'
-    }
-}
-function fecharmenu(){
-    if(abrir.style.display == 'block'){
-        abrir.style.display = 'none'
     }
 }
